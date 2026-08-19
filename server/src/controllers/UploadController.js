a1ee7c3a1b59fc743009e8f5d137108f1b82c04b
boss <khunap66@gmail.com>
2026-08-19 21:17:29 +0700

    fix: dynamic upload URL for production

--- server/src/controllers/UploadController.js
+++ server/src/controllers/UploadController.js
@@ -4,7 +4,7 @@ exports.upload = (req, res) => {
     return res.status(400).send({ error: "No file uploaded" })
   }
 
-  const url = `http://localhost:8081/assets/uploads/${req.file.filename}`
+  const url = `${req.protocol}://${req.get('host')}/assets/uploads/${req.file.filename}`
 
   res.send({
     filename: req.file.filename,
